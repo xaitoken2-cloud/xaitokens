1232a897d7977a3ef7beac44eba39f25310e7c29
xaitoken2-cloud <xaitoken2@gmail.com>
2026-08-10 08:40:53 +0500

    Delete cron.php

--- cron.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('UserFace', 1);
-include __DIR__.DIRECTORY_SEPARATOR.'core.php';
-require_once INCLUDES_PATH.'init_.php';
-
-$cron = new \Evolution\Models\Cron();
-$cron->execute(true);
